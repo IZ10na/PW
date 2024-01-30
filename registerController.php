67d40377c1abc3eb0cc36a2e3c6ea50411a93c5e
IZ10na <152287176+IZ10na@users.noreply.github.com>
2024-01-30 21:58:34 +0100

    Add files via upload

---
+++ registerController.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'userRepository.php';
+include_once 'user.php';
+
+if(isset($_POST['registerBtn'])){
+    if(empty($_POST['name']) || empty($_POST['surname']) || empty($_POST['email']) ||
+    empty($_POST['username']) || empty($_POST['password'])){
+        echo '<span style="background-color: rgb(139, 78, 37);">Fill all fields!</span>';
+
+    }else{
+        $name = $_POST['name'];
+        $surname = $_POST['surname'];
+        $email = $_POST['email'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $id = time();
+        $user = new User($id, $name, $surname, $email, $username, $password);
+
+        
+        $userRepository = new UserRepository();
+
+        $userRepository->insertUser($user);
+
+
+    }
+}
+
+
+
+?>
